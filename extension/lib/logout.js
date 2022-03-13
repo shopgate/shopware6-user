b12051f6b7e03d0653d1001a9d25d89f60b58211
Konstantin Kiritsenko <k@componentk.com>
2022-03-13 13:17:00 +0300

    SGSW6-112 logout + tests

--- extension/lib/logout.js
+++ extension/lib/logout.js
@@ -1,32 +1,32 @@
 const { decorateError } = require('./shopgate/logDecorator')
-const { setup, logout, getSessionContext } = require('@shopware-pwa/shopware-6-client')
+const { logout, getSessionContext } = require('@shopware-pwa/shopware-6-client')
+const { getContextToken } = require('./services/contextManager')
 
 /**
  * @param {SW6User.PipelineContext} context
- * @returns {Promise<void>}
+ * @returns {Promise<{contextToken: string}>}
  * @throws {Error}
  */
 module.exports = async function (context) {
-  console.log('logout called')
-  console.log('logout called')
-  console.log('logout called')
-  try {
-    await context.storage.user.del('userInfo')
-    await logout()
-  } catch (err) {
-    context.log.error(decorateError(err), 'failed to logout')
+  context.log.debug('logout called')
+  context.log.debug('logout called')
+  context.log.debug('logout called')
+
+  if (!context.meta.userId) {
+    context.log.warn('User is already logged out')
+    return {
+      contextToken: await getContextToken(context)
+    }
   }
-  try {
-    const { endpoint, accessToken, languageId } = context.config.shopware
-    setup({
-      endpoint,
-      accessToken,
-      languageId
+  await logout().catch(err => context.log.error(decorateError(err), 'failed to logout'))
+
+  // todo: should we ask for a new contextToken or load the one before login? Decision may affect the cart.
+  // todo: handle errors
+  const contextToken = await getSessionContext()
+    .then(response => response.token)
+    .catch(err => {
+      context.log.error(decorateError(err), 'failed to getSessionContext')
+      throw err
     })
-    const sessionContext = await getSessionContext()
-    return { contextToken: sessionContext.token }
-  } catch (err) {
-    context.log.error(decorateError(err), 'failed to getSessionContext')
-    throw err
-  }
+  return { contextToken }
 }
--- extension/lib/shopgate/logDecorator.js
+++ extension/lib/shopgate/logDecorator.js
@@ -3,12 +3,12 @@ module.exports = {
    * @param {Error} err
    * @param {string} [importance='high']
    * @returns {Object}
-   * */
-  decorateError(err, importance = 'high') {
+   */
+  decorateError (err, importance = 'high') {
     return {
       err,
       importance,
-      extension: '@apite-shopware6-user',
-    };
-  },
-};
+      extension: '@apite-shopware6-user'
+    }
+  }
+}
