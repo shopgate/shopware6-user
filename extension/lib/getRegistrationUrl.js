35c6ad07fa58461cb7caeaa689101213ad2690d2
Konstantin Kiritsenko <k@componentk.com>
2022-03-16 22:12:01 +0300

    SGSW6-115 frontend started - unfinished

--- extension/lib/getRegistrationUrl.js
+++ extension/lib/getRegistrationUrl.js
@@ -1,11 +1,7 @@
+const { config } = require('@shopware-pwa/shopware-6-client')
+
 /**
- * @param {SW6User.PipelineContext} context
- * @param {GetRegistrationUrlInput} input
- * @param {function} callback
+ * @return Promise<{url: string}>
  */
-
-module.exports = async function(context, input) {
-
-  // TODO-Rainer return real URL depending on config
-  return {'url': 'test.com/register', 'expires': new Date(null)}
-}
+// module.exports = async () => ({ url: config.endpoint + '/account/login' })
+module.exports = async () => ({ url: config.endpoint + '/account/login' })
