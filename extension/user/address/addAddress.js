f1ba10e62ffd2c374ed59ca516bc5cf22d16699c
Konstantin Kiritsenko <k@componentk.com>
2022-03-16 22:00:23 +0300

    SGSW6-115 removed not needed pipelines + code

--- extension/user/address/addAddress.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-/**
- * @returns {Promise<{success: boolean}>}
- */
-module.exports = async function () {
-  // TODO-Rainer send address to SW6
-  return { success: true }
-}
--- extension/user/address/updateAddress.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-/**
- * @returns {Promise<{success: boolean}>}
- */
-module.exports = async function () {
-  // TODO-Rainer update address in SW6
-  return { success: true }
-}
